c227f202f7c27872726cd3704f671811ed561564
Nataliia Pogrebna <natalibunny13@gmail.com>
2021-09-09 22:08:16 +0300

    fetch

---
+++ src/searchForm.js
@@ -0,0 +1,15 @@
+// const handleSubmit = e => {
+//   e.preventDefault();
+//   const form = e.currentTarget;
+//   query = form.querySelector('input').value;
+//   removeBtn(refs.body);
+//   clearGallery();
+//   setDefaultPage();
+//   renderGallery({ query, page: 1 });
+// };
+
+// const getForm = parentNode => {
+//   parentNode.insertAdjacentHTML('afterbegin', formMarkup);
+//   const form = document.querySelector('form');
+//   form.addEventListener('submit', handleSubmit);
+// };
