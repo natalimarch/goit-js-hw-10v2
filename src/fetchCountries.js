a31034a3f95ebcbe0e76fb700c7520d6f9eedf41
Nataliia Pogrebna <natalibunny13@gmail.com>
2021-09-10 16:19:10 +0300

    done

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,86 +1,11 @@
-import debounce from 'lodash.debounce';
 
-import { getGalleryItemMarkup } from './tmp';
 
-export const refs = {
-  body: document.body,
-  list: document.querySelector(".country-list"),
-  input: document.querySelector("#search-box"),
-  info: document.querySelector(".country-info"),
+export const fetchCountries = name => {
+  return fetch(`https://restcountries.eu/rest/v2/name/${name}`).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
 };
-
-// const getMarkupGalleryItems = country =>
-//   country.reduce((acc, el) => acc + getGalleryItemMarkup(el), '');
-  
-// const createGallery = country => {
-//   const markup = getMarkupGalleryItems(country);
-//   refs.list.insertAdjacentHTML('beforeend', markup);
-// };
-let query
-
-  const fetchCountries = ({name}) => {
-  return fetch(`https://restcountries.eu/rest/v2/name/${name}`)
-  .then(response => response.json())
-  .then(data => data)
-};
-
-
-refs.input.addEventListener('input', searchCountry);
-
-const searchCountry = e => {
-  const input = e.currentTarget;
-  query = input.querySelector('input').value;
-  fetchCountries(query)
-    .then((country) => createGallery(country))
-    .catch((error) => console.log(error));
-};
-
-function createGallery(country) {
-  const markup = names
-      .map((name) => {
-        return `<li class="country-item" id="${name}">
-        // <img class="country-flag" src="${flag}" alt="${name}">
-        // </li>`;
-      })
-      .join("");
-    refs.list.innerHTML = markup;
-}
-// const formMarkup = `<li class="country-item" id="${name}">
-// <img class="country-flag" src="${flag}" alt="${name}">
-// </li>`;
-
-// export let name = '';
-
-
-// const searchCountry = e => {
-//   const input = e.currentTarget;
-//   name = input.querySelector('input').value;
-//   fetchUsers()
-//     .then((country) => createGallery(country))
-//     .catch((error) => console.log(error));
-// };
-
-// const getForm = parentNode => {
-//     parentNode.insertAdjacentHTML('afterbegin', formMarkup);
-//     const list = document.querySelector('.country-list');
-//     list.addEventListener('input', searchCountry);
-//   };
-
-// export default getForm;
-// function searchCountry() {
-//   fetchCountries()
-//   .then((country) => createGallery(country))
-//   .catch((error) => console.log(error));
-// }
-// const renderGallery = ({ name }) =>
-//   fetchImages({ name }).then(data => {
-//     const { hits, totalHits } = data;
-//     createGallery(hits);
-//     last = Math.ceil(+totalHits / 15);
-//     if (page === 1) {
-//       removePaginator(refs.body);
-//       renderPaginator({ parentNode: refs.gallery, last });
-//     }
-//   });
-
 
--- src/index.js
+++ src/index.js
@@ -1,3 +1,91 @@
 import './css/styles.css';
+import { fetchCountries } from './fetchCountries';
 
-const DEBOUNCE_DELAY = 300;
+import debounce from 'lodash.debounce';
+import Notiflix from "notiflix";
+
+
+export const refs = {
+  DEBOUNCE_DELAY: 300,
+  body: document.body,
+  list: document.querySelector('.country-list'),
+  input: document.querySelector('#search-box'),
+  info: document.querySelector('.country-info'),
+};
+
+let query;
+
+const searchCountry = e => {
+  query = refs.input.value.trim();
+  clearInput();
+  fetchCountries(query)
+    .then(country => {createGallery(country);
+    })
+    .catch(error => {
+      Notiflix.Notify.failure('Oops, there is no country with that name');
+      console.log(error);
+    });
+};
+
+refs.input.addEventListener('input', debounce(searchCountry, refs.DEBOUNCE_DELAY));
+
+function createGallery(country) {
+  if (country.length > 10) {
+    Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
+    return;
+  }
+  console.log(country);
+  if (country.length >= 2 && country.length <= 10) {
+    const markup = country
+      .map(country => {
+        return `<li class="country-item" id="${country.name}">
+        <img class="country-flags" src="${country.flag}" alt="${country.name}"> ${country.name}
+        </li>`;
+      })
+      .join('');
+    refs.list.innerHTML = markup;
+    return markup;
+  }
+  else if (country.length === 1) {
+    const markupItem = country
+      .map(country => {
+        return `<ul class="country-list-card">
+        <li class="country-item" id="${country.name}">
+        <div class="country-header">
+        <img class="country-flag" src="${country.flag}" alt="${country.name}">
+        <p class="country-name">${country.name}</p></div>
+          <div class="descr-wrapper">
+            <p class="descr">
+              <span class="country-titles">Capital: </span>
+              <span>${country.capital}</span>
+            </p>
+            <p class="descr">
+              <span class="country-titles">Population: </span>
+              <span>${country.population}</span>
+            </p>
+            <p class="descr">
+              <span>Language: </span>
+              ${country.languages.length > 1 ?
+                `<ul class ="language_list"> 
+                ${country.languages.map(language => {
+                  return `<li class = "language_item">${language.name}</li>`
+                }).join("")}
+                </ul>` : `${country.languages[0].name}`}
+            </p>
+          </div>
+        </li>
+        </ul>`
+      }).join('');
+    refs.info.innerHTML = markupItem;
+  }
+}
+
+
+function clearInput() {
+  if (refs.list.children.length) {
+    refs.list.innerHTML = '';
+  } else if(refs.info.children.length) {
+    refs.info.innerHTML = '';
+  }
+}
+
